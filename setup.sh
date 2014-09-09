b07a103127cf794d240a70395b3fad8f131f125e
openi-fokus <openi@fokus.fraunhofer.de>
2014-09-09 19:32:32 +0200

    dropped table on setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ PATH=$JAVA_HOME/bin:$PATH
 
 bash gradlew war
 
+echo "drop table authz_approvals; drop table authz_approvals_old; drop table expiring_code_store; drop table external_group_mapping; drop table group_membership; drop table groups; drop table oauth_client_details; drop table oauth_code; drop table schema_version; drop table sec_audit; drop table users;" | psql -U postgres -d uaa
+
 sudo rm -R /var/lib/tomcat7/webapps/uaa*
 sudo cp uaa/build/libs/cloudfoundry-identity-uaa-1.8.3.war /var/lib/tomcat7/webapps/uaa.war
 
