5e0009fec1a76a9bae2731fa93aea17cd360ea89
openi-fokus <openi@fokus.fraunhofer.de>
2014-09-09 20:11:48 +0000

    dev setup improved

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ PATH=$JAVA_HOME/bin:$PATH
 
 bash gradlew war
 
-cp uaa/build/libs/cloudfoundry-identity-uaa-1.8.3.war /var/lib/tomcat7/webapps/
+echo "drop table authz_approvals; drop table authz_approvals_old; drop table expiring_code_store; drop table external_group_mapping; drop table group_membership; drop table groups; drop table oauth_client_details; drop table oauth_code; drop table schema_version; drop table sec_audit; drop table users;" | psql -U postgres -d uaa
+
+sudo cp uaa/build/libs/cloudfoundry-identity-uaa-1.8.3.war /var/lib/tomcat7/webapps/uaa.war
 
 cd proxy
 npm install --no-bin-links
